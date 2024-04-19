6bff840ca6c33bb165258ac8948a0606353a0d43
“bhavadharani16” <bhavadharaniep10@gmail.com>
2024-04-19 07:29:04 +0000

    First code commit

---
+++ Pro.java
@@ -0,0 +1,94 @@
+SDLC Happy 
+	software development life cycle
+1) Planning
+2) requirement
+3) desining
+4) development
+5) testing
+6) deployment --->devops
+7) maintainence
+
+This project is about how devops use ci cd pipeline for automation of SDLC
+DevOps is a set of practice that combines software development and IT operation. which aims to 
+shorten the software life cycle and deliver high quality software continuosly
+OPS---> managing and maintaing IT infrastructure 
+DEV ----> creating, building, testing 
+
+Automation: Automation is a fundamental principle of DevOps. It involves using tools 
+and scripts to automate manual and repetitive tasks, such as code building, testing, deployment,
+and infrastructure provisioning. Automation helps increase efficiency, reduce errors, and ensure consistency in processes. 
+
+Here we are using jenkins as an automation tool.
+Jenkins is an open-source automation server  It allows developers to automate repetitive tasks and streamline the 
+continuous integration and continuous delivery (CI/CD) processes.
+
+Continuous Integration (CI): Optimizes building, integrating, and testing code within a development environment.
+Continuous Delivery (CD): after the testing the cd makes sure that is always in deployable state that is ready for production
+
+
+Why ci cd well it automates things would have to be done automatically and it finds out error easily 
+
+shorter cycle time this is the speed at which devops team can deliver a functional application
+from the moment work begins and also provide values to the end user and it also leads to happy employees and 
+it also get to market faster because the code that has to run through the pipeline and now in production
+is now making money whereas code sitting on a harddrive does nothing
+
+
+
+--------------------------------------------------------
+OOPS 
+	Object-Oriented Programming (OOP) is a programming It helps us to structure our code in an organized way. 
+In oop the code is organized around the data(attributes) and the operator(methods) that can be performed on objects
+
+Use of OOP in Python:
+
+Modularity: OOP allows you to break down a large program into smaller, manageable parts (classes). Each class handles a specific aspect of functionality.
+Reusability: Once you define a class, you can create multiple objects from it. This reusability reduces redundant code.
+Maintainability: OOP promotes clean code organization, making it easier to maintain and debug.
+Scalability: As your program grows, OOP helps you add new features by extending existing classes or creating new ones.
+Design Patterns: OOP aligns with common design patterns (e.g., Singleton, Factory, Observer), making it easier to implement them in Python.
+
+The primary principles of OOP are encapsulation, inheritance, and polymorphism. 
+
+A constructor is a member function of a class that is used to create objects of that class. 
+It has the same name as the class itself, has no return type, and is invoked using the new operator.
+
+A method is an ordinary member function of a class. 
+It has its own name, a return type (which may be void), and is invoked using the dot operator.
+
+----------------------------------------
+Polymorphism:
+
+Definition: Polymorphism allows objects of different types to be treated as objects of a common type. 
+It includes method overloading and method overriding.
+
+Use: Polymorphism promotes flexibility and adaptability in code. 
+It allows a single interface to be used for different types of objects, 
+making it easier to write code that can work with a variety of objects without needing to know their specific types.
+
+----------------------------------------------
+
+Serverless computing, often referred to as Function as a Service (FaaS), is a cloud computing model where cloud providers automatically 
+manage the infrastructure for running and scaling applications. In a serverless architecture, developers can focus on writing code 
+for individual functions or services without dealing with the complexities of managing servers, infrastructure provisioning, or system maintenance.
+
+Microservices: Building and deploying individual functions as microservices.
+
+Event-Driven Applications: Handling events and triggers in response to various activities.
+
+Backend for Mobile and Web Applications: Creating scalable backends for applications without managing servers.
+
+Data Processing: Executing code in response to data changes or scheduled tasks.
+------------------------
+CLOUD COMPUTING
+	Cloud computing is a general term for the delivery of hosted computing services and IT resources over the internet with pay-as-you-go pricing. 
+Users can obtain technology services such as processing power, storage and databases from a cloud provider, eliminating the need for purchasing, 
+operating and maintaining on-premises physical data centers and servers.
+
+--------------------------
+A cloud can be private, public or a hybrid. 
+A public cloud sells services to anyone on the internet.
+A private cloud is a proprietary network or a data center that supplies hosted services to a limited number of people, with certain access and permissions settings.
+A hybrid cloud offers a mixed computing environment where data and resources can be shared between both public and private clouds.
+
+---------------------
